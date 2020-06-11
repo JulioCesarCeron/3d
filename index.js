8b9f11efeb89d08c98c0084f032429ac6eb51019
julio.ceron <julio.ceorn@gmail.com>
2020-06-11 02:01:02 -0300

    cube example

--- index.js
+++ index.js
@@ -1,87 +1,51 @@
-let camera = new THREE.PerspectiveCamera(
-	40,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
-);
-camera.position.z = 150;
+let camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 let scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x111111 );
 scene.fog = new THREE.Fog(0x111111, 150, 200);
 
+let material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+let geometry = new THREE.CubeGeometry(20, 20, 20);
+let mesh = new THREE.Mesh(geometry, material);
 
-// -------
-let subdivisions = 6;
-let recursion = 1;
+scene.add(mesh);
 
-/* let points = GeometryUtils.hilbert3D(
-	new THREE.Vector3( 0, 0, 0 ),
-	25.0,
-	recursion,
-	0,
-	1,
-	2,
-	3,
-	4,
-	5,
-	6,
-	7
-);
-
-let spline = new THREE.CatmullRomCurve3( points );
-let samples = spline.getPoints( points.length * subdivisions );
-var geometrySpline = new THREE.BufferGeometry().setFromPoints( samples ); */
-var geometrySpline = new THREE.BufferGeometry().setFromPoints();
-
-var line = new THREE.Line(
-	geometrySpline,
-	new THREE.LineDashedMaterial({
-		color: 0xffffff,
-		dashSize: 1,
-		gapSize: 0.5
-	})
-);
-line.computeLineDistances();
-
-scene.add( line );
-
-var geometryBox = box(
-	50,
-	50,
-	50
-);
-
-let material = new THREE.LineDashedMaterial({
-	color: 0xffaa00,
-	dashSize: 3,
-	gapSize: 1 
-}); 
-
-var lineSegments = new THREE.LineSegments(
-	geometryBox,
-	material
-);
-lineSegments.computeLineDistances();
-
-scene.add( lineSegments );
-// -------
-
-
-let renderer = new THREE.WebGLRenderer({
-	antialias: true
-});
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio( window.devicePixelRatio );
 
 renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 
+let clock = new THREE.Clock();
+
+
+const onWindowResize = () => {
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener( 'resize', onWindowResize, false );
+
 let animate = function () {
+	let dir = 1;
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	let delta = clock.getDelta();
+
+	mesh.rotation.y += delta * 0.5;	
+	mesh.rotation.x += delta * 2;
+	mesh.rotation.y += dir * delta;
+	if( mesh.position.x > 2 ) {
+		dir = -1;
+	} else if ( mesh.position.x < -2) {
+		dir = 1;
+	}
 
 	renderer.render( scene, camera );
 };
