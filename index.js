130ad1f6e25c2eca15ef330bef915460761de60e
julio.ceron <julio.ceorn@gmail.com>
2020-06-11 02:07:50 -0300

    set position camera

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 100);
+camera.position.set(-100, 50, 100);
 camera.lookAt(0, 0, 0);
 
 let scene = new THREE.Scene();
@@ -9,6 +9,7 @@ scene.fog = new THREE.Fog(0x111111, 150, 200);
 let material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
 let geometry = new THREE.CubeGeometry(20, 20, 20);
 let mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
 
 scene.add(mesh);
 
@@ -38,9 +39,9 @@ let animate = function () {
 
 	let delta = clock.getDelta();
 
-	mesh.rotation.y += delta * 0.5;	
-	mesh.rotation.x += delta * 2;
-	mesh.rotation.y += dir * delta;
+	 mesh.rotation.y += delta * 0.5;	
+	// mesh.rotation.x += delta * 0.5;
+	// mesh.rotation.y += dir * delta;
 	if( mesh.position.x > 2 ) {
 		dir = -1;
 	} else if ( mesh.position.x < -2) {
