950046dec02079299e0b0d974e306f1a012e242b
Julio Ceron <julio.ceron@endurance.com>
2020-09-11 18:43:11 -0300

    WIP - add webpack configs

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-	mode: 'production'
-  output: {
-    filename: '[name].[contentHash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
